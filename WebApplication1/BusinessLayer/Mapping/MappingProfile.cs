38cc5bd51f4815bfb6ca5938d8360d91ed11e674
Whitetail11 <43177664+Whitetail11@users.noreply.github.com>
2021-02-01 08:46:55 -0800

    Create MappingProfile.cs

---
+++ WebApplication1/BusinessLayer/Mapping/MappingProfile.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using BusinessLayer.DTOs;
+using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessLayer.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Book, BookDTO>()
+                .ForMember(j => j.Authors, opt => opt.MapFrom(
+                    route => route.BookAuthors.ToList().Select(
+                        el => new AuthorDTO
+                        {
+                            Name = el.Authors.Name,
+                            Id = el.Authors.Id
+                        }
+                        )))
+                .ForMember(j => j.Users, opt => opt.MapFrom(
+                    route => route.UsersBooks.ToList().Select(
+                        el => new UserDTO
+                        {
+                            Name = el.User.Name
+                        }
+                        )
+                    ));
+
+        }
+    }
+}

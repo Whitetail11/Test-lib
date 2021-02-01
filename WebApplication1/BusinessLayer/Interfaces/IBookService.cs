637630aab4406aa7d81fcfbb0d904f57787972b5
Whitetail11 <43177664+Whitetail11@users.noreply.github.com>
2021-02-01 08:47:04 -0800

    create services

---
+++ WebApplication1/BusinessLayer/Interfaces/IBookService.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Interfaces
+{
+    public interface IBookService
+    {
+        ICollection<BookDTO> GetBooks();
+        BookDTO GetById(int id);
+        void TakeBook(int bookId);
+        void ReturnBook(int bookId);
+    }
+}
---
+++ WebApplication1/BusinessLayer/Services/BookService.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using BusinessLayer.DTOs;
+using BusinessLayer.Interfaces;
+using DataLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly IMapper mapper;
+        private readonly IBookRepository bookRepository;
+
+        public BookService(IMapper mapper, IBookRepository bookRepository)
+        {
+            this.mapper = mapper;
+            this.bookRepository = bookRepository;
+        }
+        public ICollection<BookDTO> GetBooks()
+        {
+            return mapper.Map<ICollection<BookDTO>>(this.bookRepository.GetBooks());
+        }
+
+        public BookDTO GetById(int id)
+        {
+            return mapper.Map<BookDTO>(this.bookRepository.GetById(id));
+        }
+
+        public void ReturnBook(int bookId)
+        {
+            this.bookRepository.ReturnBook(bookId);
+        }
+
+        public void TakeBook(int bookId)
+        {
+            this.bookRepository.TakeBook(bookId);
+        }
+    }
+}

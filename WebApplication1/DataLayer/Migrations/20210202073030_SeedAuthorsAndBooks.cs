9da936d850a764e53251a6acbd13f05946806754
Whitetail11 <43177664+Whitetail11@users.noreply.github.com>
2021-02-02 00:43:36 -0800

    seed books and authors

---
+++ WebApplication1/DataLayer/Migrations/20210202073030_SeedAuthorsAndBooks.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataLayer.Migrations
+{
+    public partial class SeedAuthorsAndBooks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Стивен Кинг" },
+                    { 2, "Стивен Хокинг" },
+                    { 3, "Вася" },
+                    { 4, "Петя" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Amount", "Available", "Name" },
+                values: new object[,]
+                {
+                    { 1, 4, 4, "Краткая история времени" },
+                    { 2, 2, 2, "Сияние" },
+                    { 3, 3, 3, "Математика" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "GetBookAuthors",
+                columns: new[] { "AuthorId", "BookId" },
+                values: new object[,]
+                {
+                    { 2, 1 },
+                    { 1, 2 },
+                    { 3, 3 },
+                    { 4, 3 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "GetBookAuthors",
+                keyColumns: new[] { "AuthorId", "BookId" },
+                keyValues: new object[] { 1, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "GetBookAuthors",
+                keyColumns: new[] { "AuthorId", "BookId" },
+                keyValues: new object[] { 2, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "GetBookAuthors",
+                keyColumns: new[] { "AuthorId", "BookId" },
+                keyValues: new object[] { 3, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "GetBookAuthors",
+                keyColumns: new[] { "AuthorId", "BookId" },
+                keyValues: new object[] { 4, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
---
+++ WebApplication1/DataLayer/ModelBuilderExtensions.cs
@@ -0,0 +1,82 @@
+﻿using DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().HasData(
+                new Book
+                {
+                    Id = 1,
+                    Name = "Краткая история времени",
+                    Amount = 4,
+                    Available = 4
+                },
+                new Book
+                {
+                    Id = 2,
+                    Name = "Сияние",
+                    Amount = 2,
+                    Available = 2
+                },
+                new Book
+                {
+                    Id = 3,
+                    Name = "Математика",
+                    Amount = 3,
+                    Available = 3
+                }
+                );
+            modelBuilder.Entity<Author>().HasData(
+                new Author
+                {
+                    Id = 1,
+                    Name = "Стивен Кинг"
+                },
+                new Author
+                {
+                    Id = 2,
+                    Name = "Стивен Хокинг"
+                },
+                new Author
+                {
+                    Id = 3,
+                    Name = "Вася"
+                },
+                new Author
+                {
+                    Id = 4,
+                    Name = "Петя"
+                }
+                );
+            modelBuilder.Entity<BookAuthor>().HasData(
+                new BookAuthor
+                {
+                    AuthorId = 1,
+                    BookId = 2
+                },
+                new BookAuthor
+                {
+                    AuthorId = 2,
+                    BookId = 1
+                },
+                new BookAuthor
+                {
+                    AuthorId = 3,
+                    BookId = 3,
+                },
+                new BookAuthor
+                {
+                    AuthorId = 4,
+                    BookId = 3
+                }
+                );
+        }
+    }
+}
